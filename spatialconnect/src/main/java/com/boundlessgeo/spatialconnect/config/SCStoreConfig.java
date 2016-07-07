45563a02338af3e29e4e717f3ec7a9e17ae5b31a
Marc Cenac <mcenac@boundlessgeo.com>
2016-07-07 15:19:56 -0500

    add default layer to store config props

--- spatialconnect/src/main/java/com/boundlessgeo/spatialconnect/config/SCStoreConfig.java
+++ spatialconnect/src/main/java/com/boundlessgeo/spatialconnect/config/SCStoreConfig.java
@@ -56,6 +56,12 @@ public class SCStoreConfig
      */
     private String name;
 
+    /**
+     * Optionally specify a default layer for this store
+     */
+    @JsonProperty("default_layer")
+    private String defaultLayer;
+
     public SCStoreConfig() {}
 
     public String getType()
@@ -111,5 +117,13 @@ public class SCStoreConfig
     public void setName(String name)
     {
         this.name = name;
+    }
+
+    public String getDefaultLayer() {
+        return defaultLayer;
+    }
+
+    public void setDefaultLayer(String defaultLayer) {
+        this.defaultLayer = defaultLayer;
     }
 }
